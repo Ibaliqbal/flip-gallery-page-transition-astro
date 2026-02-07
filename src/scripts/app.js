09775eee251708adcab0712d100602d9f7451263
Ibaliqbal <iqbalmuthahhary@gmail.com>
2026-02-08 00:21:26 +0700

    finsih project

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import Scroll from "./lib/scroll";
 import barba from "@barba/core";
 import { Flip } from "gsap/Flip";
-import { getDetailSlugNamespace, select, selectAll } from "./utils";
+import { loadImages, select, selectAll } from "./utils";
 import { data as dataImage } from "./data";
 
 gsap.registerPlugin(Flip);
@@ -28,28 +28,120 @@ class App {
 
   setPageTranition() {
     barba.init({
-      prefetchIgnore: true,
       transitions: [
         {
           name: "default-transition",
-          before: (data) => {
-            console.log("🔥 default transition");
+          before: () => {
+            console.log("🔥 default transition 🔥");
             this.scroll.stop();
+            const barbaPageTransition = select(".barba__page__transition");
+            const barbaPageTransitionBar = selectAll(
+              ".barba__page__transition__bar",
+            );
+            const barbaPageTransitionProgress = select(
+              ".barba__page__transition__progress",
+            );
+
+            gsap.set(barbaPageTransitionBar, {
+              transformOrigin: "bottom",
+            });
+
+            gsap.set(barbaPageTransitionProgress, {
+              transformOrigin: "left",
+            });
+
+            const timeline = gsap.timeline();
+
+            gsap.set(barbaPageTransition, {
+              visibility: "visible",
+              opacity: 1,
+              pointerEvents: "all",
+            });
+
+            timeline.to(barbaPageTransitionBar, {
+              scaleY: 1,
+              stagger: { from: "center", amount: 0.35 },
+              ease: "power2.inOut",
+            });
+
+            timeline.to(
+              barbaPageTransitionProgress,
+              {
+                scaleX: 1,
+                duration: 0.5,
+                ease: "power2.inOut",
+              },
+              "-=0.2",
+            );
+
+            return new Promise((resolve) => {
+              timeline.call(() => {
+                resolve();
+              });
+            });
           },
           leave: () => {
-            console.log("leave");
-          },
-          beforeEnter: () => {
             this.scroll.destroy();
           },
           after: () => {
             this.scroll.init();
             this.scroll.scrollTop(true);
+            const template = this.getCurrentTemplate();
+            this.setTemplate(template);
+
+            const barbaPageTransition = select(".barba__page__transition");
+            const barbaPageTransitionBar = selectAll(
+              ".barba__page__transition__bar",
+            );
+            const barbaPageTransitionProgress = select(
+              ".barba__page__transition__progress",
+            );
+
+            const timeline = gsap.timeline({
+              onComplete: () => {
+                gsap.set(barbaPageTransition, {
+                  visibility: "hidden",
+                  opacity: 0,
+                  pointerEvents: "none",
+                });
+              },
+            });
+
+            gsap.set(barbaPageTransitionBar, {
+              transformOrigin: "top",
+            });
+
+            gsap.set(barbaPageTransitionProgress, {
+              transformOrigin: "right",
+            });
+
+            timeline.to(barbaPageTransitionProgress, {
+              scaleX: 0,
+              ease: "power2.inOut",
+              duration: 0.4,
+            });
+
+            timeline.to(
+              barbaPageTransitionBar,
+              {
+                scaleY: 0,
+                stagger: {
+                  from: "center",
+                  amount: 0.35,
+                },
+                ease: "power2.inOut",
+              },
+              "-=0.2",
+            );
+            return new Promise((resolve) => {
+              timeline.call(() => {
+                resolve();
+              });
+            });
           },
         },
         {
           name: "detail-to-detail",
-
           from: {
             namespace: dataImage.map((item) => `detail__${item.slug}`),
           },
@@ -57,36 +149,38 @@ class App {
             namespace: dataImage.map((item) => `detail__${item.slug}`),
           },
           before: (data) => {
-            console.log("🔥 detail to detail transition");
+            console.log("🔥 detail to detail transition 🔥");
             this.scroll.stop();
+            const timeline = gsap.timeline();
+
+            const allItems = [...selectAll(".item__image__outer")];
+            const nextEl = select(".next__item");
+
+            timeline.to(allItems, {
+              opacity: 0.001,
+              duration: 0.5,
+              ease: "power4.inOut",
+            });
+
+            if (typeof data.trigger === "string")
+              timeline.to(
+                nextEl,
+                {
+                  opacity: 0.001,
+                  duration: 0.5,
+                  ease: "power4.inOut",
+                },
+                0,
+              );
 
             return new Promise((resolve) => {
-              const timeline = gsap.timeline({
-                onComplete: resolve,
+              timeline.call(() => {
+                resolve();
               });
-
-              const allItems = [...selectAll(".item__image__outer")];
-              const nextEl = select(".next__item");
-
-              timeline.to(allItems, {
-                opacity: 0.001,
-                duration: 0.5,
-                ease: "power4.inOut",
-              });
-
-              if (typeof data.trigger === "string")
-                timeline.to(
-                  nextEl,
-                  {
-                    opacity: 0.001,
-                    duration: 0.5,
-                    ease: "power4.inOut",
-                  },
-                  0,
-                );
             });
           },
           leave: (data) => {
+            this.scroll.destroy();
             if (typeof data.trigger !== "string") {
               const triggerImage = data.trigger.querySelector("img");
 
@@ -121,108 +215,107 @@ class App {
               };
             }
           },
-          beforeEnter: () => {
-            this.scroll.destroy();
-          },
           after: (data) => {
             this.scroll.init();
             this.scroll.scrollTop(true);
             const allItems = [...selectAll(".item__image__outer")];
+            const template = this.getCurrentTemplate();
+            this.setTemplate(template);
 
-            return new Promise((resolve) => {
-              const timeline = gsap.timeline({
-                onComplete: resolve,
-              });
+            const timeline = gsap.timeline();
 
-              if (typeof data.trigger === "string") {
-                timeline.fromTo(
-                  allItems,
-                  {
-                    opacity: 0.001,
-                  },
-                  {
-                    opacity: 1,
-                    duration: 0.75,
-                    ease: "power4.inOut",
-                  },
-                );
+            if (typeof data.trigger === "string") {
+              timeline.fromTo(
+                allItems,
+                {
+                  opacity: 0.001,
+                },
+                {
+                  opacity: 1,
+                  duration: 0.75,
+                  ease: "power4.inOut",
+                },
+              );
 
-                timeline.fromTo(
-                  ".next__item",
-                  { opacity: 0.001 },
-                  { opacity: 1, duration: 0.75, ease: "power4.inOut" },
-                  "<",
-                );
-              } else {
-                const restItem = [];
-                allItems.forEach((item, idx) => {
-                  const innerEl = item.querySelector(".item__image__inner");
+              timeline.fromTo(
+                ".next__item",
+                { opacity: 0.001 },
+                { opacity: 1, duration: 0.75, ease: "power4.inOut" },
+                "<",
+              );
+            } else {
+              const restItem = [];
+              allItems.forEach((item, idx) => {
+                const innerEl = item.querySelector(".item__image__inner");
 
-                  if (!innerEl) return;
+                if (!innerEl) return;
 
-                  if (
-                    idx === 0 &&
-                    this.flipNextEl.outer &&
-                    this.flipNextEl.inner
-                  ) {
-                    gsap.set(item, {
-                      visibility: "hidden",
-                    });
+                if (
+                  idx === 0 &&
+                  this.flipNextEl.outer &&
+                  this.flipNextEl.inner
+                ) {
+                  gsap.set(item, {
+                    visibility: "hidden",
+                  });
 
-                    Flip.fit(innerEl, this.flipNextEl.inner, {
+                  Flip.fit(innerEl, this.flipNextEl.inner, {
+                    scale: true,
+                  });
+
+                  gsap.set(item, {
+                    visibility: "visible",
+                  });
+
+                  gsap.set(this.flipNextEl.inner, {
+                    visibility: "hidden",
+                  });
+
+                  timeline.call(() => {
+                    Flip.fit(innerEl, item, {
                       scale: true,
+                      duration: 1.45,
+                      ease: "power4.inOut",
+                      onComplete: () => {
+                        this.flipNextEl.outer.remove();
+                        this.flipNextEl = {
+                          outer: null,
+                          inner: null,
+                        };
+                      },
                     });
-
-                    gsap.set(item, {
-                      visibility: "visible",
-                    });
-
-                    gsap.set(this.flipNextEl.inner, {
-                      visibility: "hidden",
-                    });
-
-                    timeline.call(() => {
-                      Flip.fit(innerEl, item, {
-                        scale: true,
-                        duration: 1.45,
-                        ease: "power4.inOut",
-                        onComplete: () => {
-                          this.flipNextEl.outer.remove();
-                          this.flipNextEl = {
-                            outer: null,
-                            inner: null,
-                          };
-                        },
-                      });
-                    });
-                  } else {
-                    restItem.push(item);
-                  }
-                });
-                timeline.fromTo(
-                  restItem,
-                  {
-                    opacity: 0.001,
-                  },
-                  {
-                    opacity: 1,
-                    duration: 0.75,
-                    ease: "power4.inOut",
-                  },
-                  "+=0.55",
-                );
-                timeline.fromTo(
-                  ".next__item",
-                  { opacity: 0.001 },
-                  { opacity: 1, duration: 0.75, ease: "power4.inOut" },
-                  "<",
-                );
-              }
+                  });
+                } else {
+                  restItem.push(item);
+                }
+              });
+              timeline.fromTo(
+                restItem,
+                {
+                  opacity: 0.001,
+                },
+                {
+                  opacity: 1,
+                  duration: 0.75,
+                  ease: "power4.inOut",
+                },
+                "+=0.75",
+              );
+              timeline.fromTo(
+                ".next__item",
+                { opacity: 0.001 },
+                { opacity: 1, duration: 0.75, ease: "power4.inOut" },
+                "<",
+              );
+            }
+            return new Promise((resolve) => {
+              timeline.call(() => {
+                resolve();
+              });
             });
           },
         },
       ],
-      debug: true,
     });
   }
 
@@ -234,7 +327,83 @@ class App {
     this.template = template;
   }
 
-  intro() {}
+  intro() {
+    if (this.template === "home") this.homeInit();
+    if (this.template === "detail") this.detailInit();
+  }
+
+  homeInit() {
+    console.log("🔥 home init 🔥");
+    const gridItems = [...selectAll(".grid__item")];
+    gridItems.forEach((item) => item.classList.add("load"));
+
+    gsap.set(gridItems, {
+      scale: 0,
+      pointerEvents: "none",
+    });
+
+    const timeline = gsap.timeline();
+
+    timeline.to(gridItems, {
+      scale: 1,
+      ease: "power4.inOut",
+      duration: 1.75,
+      stagger: {
+        each: 0.25,
+      },
+      onComplete: () => {
+        gsap.set(gridItems, {
+          clearProps: "transform",
+        });
+      },
+    });
+
+    timeline.add(() => {
+      const gridState = Flip.getState(gridItems);
+
+      gridItems.forEach((item) => {
+        item.classList.remove("load");
+      });
+
+      Flip.from(
+        gridState,
+        {
+          scale: true,
+          ease: "power1",
+          duration: 0.75,
+          onComplete: () => {
+            gsap.set(gridItems, {
+              pointerEvents: "all",
+            });
+            timeline.kill();
+          },
+          stagger: 0.1345,
+        },
+        "+=0.5",
+      );
+    });
+  }
+
+  detailInit() {
+    console.log("🔥 detail init 🔥");
+    const allItems = [...selectAll(".item__image__outer")];
+
+    gsap.fromTo(
+      allItems,
+      {
+        opacity: 0.001,
+      },
+      {
+        opacity: 1,
+        duration: 0.75,
+        ease: "power4.inOut",
+      },
+    );
+  }
 }
 
-new App();
+const app = new App();
+
+loadImages(() => {
+  // app.intro();
+});
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,5 +1,29 @@
 export const select = (selector) => document.querySelector(selector);
 export const selectAll = (selector) => document.querySelectorAll(selector);
 
-export const getDetailSlugNamespace = (namespace) =>
-  !namespace.startWith("detail__") ? null : namespace.split("detail__")[1];
+export function loadImages(callback) {
+  const images = selectAll("img");
+
+  if (!images.length) {
+    callback();
+    return;
+  }
+
+  let loaded = 0;
+
+  const onLoad = () => {
+    loaded++;
+    if (loaded === images.length) {
+      callback();
+    }
+  };
+
+  images.forEach((img) => {
+    if (img.complete && img.naturalWidth !== 0) {
+      onLoad(); // image sudah ready
+    } else {
+      img.addEventListener("load", onLoad, { once: true });
+      img.addEventListener("error", onLoad, { once: true });
+    }
+  });
+}
